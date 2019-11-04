912fc5fcebca6baf72387dfa4281b12f053e34a0
mahesh Paruchuri <saisivap289@gmail.com>
2019-11-04 16:57:39 +0700

    Modified the admin to a defalut false

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_02_141723) do
+ActiveRecord::Schema.define(version: 2019_11_04_095323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 2019_11_02_141723) do
     t.string "last_name"
     t.string "stID"
     t.integer "balance"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "phone_number"
     t.string "country"
     t.index ["email"], name: "index_users_on_email", unique: true
