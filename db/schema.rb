baeba7be8f81c7c9cab309f4cbf6db5f76e942d4
mahesh Paruchuri <saisivap289@gmail.com>
2019-11-06 19:42:07 +0700

    Added

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_095323) do
+ActiveRecord::Schema.define(version: 2019_11_06_112201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "category"
+    t.float "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "quotations", force: :cascade do |t|
     t.string "author_name"
